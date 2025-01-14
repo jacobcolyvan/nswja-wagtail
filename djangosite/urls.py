e36ae9f8e92dfdf21401d90ca48923d4a3f5a89b
Jacob Colyvan <jacob@interaction.net.au>
2025-01-15 00:52:58 +1100

    Add sitemap.xml

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -4,12 +4,14 @@ from django.urls import include, path
 from django.views.generic import TemplateView
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.contrib.sitemaps.views import sitemap
 from wagtail.documents import urls as wagtaildocs_urls
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
+    path("sitemap.xml", sitemap),
 ]
 
 if settings.DEBUG:
